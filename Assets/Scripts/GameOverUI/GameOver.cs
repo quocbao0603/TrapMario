4ca98d2790c85f390b3776257b9866b988596d4d
baoz <qbao0603@gmail.com>
2021-11-18 17:02:20 +0700

    stop 3.5s when game over

--- Assets/Scripts/GameOverUI/GameOver.cs
+++ Assets/Scripts/GameOverUI/GameOver.cs
@@ -6,9 +6,12 @@ using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
 {
+    public bool updateOn = true;
     public GameObject dieCounterUI;
     // Start is called before the first frame update
     void Start() {
+       
+        StartCoroutine (updateOff());
         Debug.Log(DataManagement.dataManagement.dies_counter);
     }
 
@@ -18,10 +21,12 @@ public class GameOver : MonoBehaviour
         int dies_counter = DataManagement.dataManagement.dies_counter;
         // Set number of dies timer to UI
         dieCounterUI.gameObject.GetComponent<Text>().text = ("x -" + dies_counter);
-        if (Input.anyKeyDown)
-        {
-            if (!Input.GetKey(KeyCode.Mouse0) && !Input.GetKey(KeyCode.Mouse1)) { 
-                SceneManager.LoadScene("GameLevel1");
+        if (updateOn == true){
+            if (Input.anyKeyDown)
+            {
+                if (!Input.GetKey(KeyCode.Mouse0) && !Input.GetKey(KeyCode.Mouse1)) { 
+                    SceneManager.LoadScene("GameLevel1");
+                }
             }
         }
     }
@@ -32,4 +37,13 @@ public class GameOver : MonoBehaviour
         SceneManager.LoadScene("MainMenu");
     }
 
+    IEnumerator updateOff ()
+    {
+        Debug.Log("updateOff");
+        updateOn = false;
+        yield return new WaitForSeconds (3.5f);
+        updateOn = true;
+    }
 }
+
+
